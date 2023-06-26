aaa1b5273ff4a12383fd90fafb5d08e69775c7d7
Anish Singh <anishsingh02@hotmail.com>
2023-06-26 20:42:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import 'https://github.com/anishhhsingh/starwarss/edit/main/index.css';
+import App from 'https://github.com/anishhhsingh/starwarss/edit/main/App';
+import reportWebVitals from 'https://github.com/anishhhsingh/starwarss/edit/main/reportWebVitals';
 import 'semantic-ui-css/semantic.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
